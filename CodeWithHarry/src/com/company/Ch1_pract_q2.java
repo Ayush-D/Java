66933b76a0047280d3d917d5826291fc6377c546
Ayush <https://ayush1111@bitbucket.org/fcb_ved/user_app.git>
2021-01-20 23:02:04 +0530

    Files Commited

---
+++ CodeWithHarry/src/com/company/Ch1_pract_q2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Ch1_pract_q2 {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++)
+            System.out.println("Welcome to Java");
+
+    }
+}
