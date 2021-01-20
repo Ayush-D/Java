b868c95ac841652ba5df474dba9a2d47651610dc
Ayush <https://ayush1111@bitbucket.org/fcb_ved/user_app.git>
2021-01-20 23:45:10 +0530

    Chapter 1

--- CodeWithHarry/src/com/company/Ch1_pract_q4.java
+++ CodeWithHarry/src/com/company/Ch1_pract_q4.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Ch2_pract_q4 {
+public class Ch1_pract_q4 {
     public static void main(String[] args) {
         System.out.println("a\ta^2\ta^3");
         for(int i = 1; i <= 4; i++)
--- CodeWithHarry/src/com/company/Ch1_pract_q6.java
+++ CodeWithHarry/src/com/company/Ch1_pract_q6.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Ch2_pract_q6 {
+public class Ch1_pract_q6 {
     public static void main(String[] args) {
         for(int i = 1; i <= 8; i++)
             System.out.printf("%d + ", i);
