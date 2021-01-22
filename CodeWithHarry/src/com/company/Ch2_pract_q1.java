99dfe934e919cf42f886be109fec3fa6c7adc5f1
Ayush <https://ayush1111@bitbucket.org/fcb_ved/user_app.git>
2021-01-22 20:01:28 +0530

    Committed Files

---
+++ CodeWithHarry/src/com/company/Ch2_pract_q1.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Ch2_pract_q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer num = sc.nextInt();
+    }
+}
